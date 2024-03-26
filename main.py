3d31c15b555c19a5ea084f6032d93912fa56cfab
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-03-26 11:26:47 +0200

    Basics: printing to the console log, string manipulation and debugging.

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+print("Hello World!\nHello World!")
+print("Hello" + " " + "Vugar")
+print("Hello " +  "Vugar")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# Fixed the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
