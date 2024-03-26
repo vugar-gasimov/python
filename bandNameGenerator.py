8564ddd5432c87ecc9cbdf779171247eadbffcf9
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-03-26 13:27:05 +0200

    Small update in space

--- bandNameGenerator.py
+++ bandNameGenerator.py
@@ -1,21 +1,21 @@
 #1. Erstellen Sie eine Begrüßung für Ihr Programm.
 print(
-     "Willkommen bei Vugars erster App, die einen Bandnamen generiert, und leider auch blöd aussieht =)"
+     "Willkommen bei Vugars erster App, die einen Bandnamen generiert, und leider auch blöd aussieht =) \n"
 )
 #2. Fragen Sie den Benutzer nach der Stadt, in der er aufgewachsen ist.
-users_name = input("Wie ist Ihr Name, lieber Benutzer? ")
-print("Freut mich, Sie kennenzulernen, Schatz " + users_name)
+users_name = input("Wie ist Ihr Name, lieber Benutzer? \n")
+print("Freut mich, Sie kennenzulernen, Schatz " + users_name )
 user_city = input(
-     "Wenn es kein Geheimnis ist, könnten Sie mir dann sagen, wie die Stadt heißt, in der Sie aufgewachsen sind? "
+     "Wenn es kein Geheimnis ist, könnten Sie mir dann sagen, wie die Stadt heißt, in der Sie aufgewachsen sind? \n"
 )
 #3. Fragen Sie den Benutzer nach dem Namen eines Haustieres.
 users_pet_name = input(
-     "Wenn Sie ein Haustier haben oder hatten, wie heißt es? (Wenn Sie nie ein Haustier gehabt hätten, wie würden Sie es nennen, wenn Sie es hätten?) "
+     "Wenn Sie ein Haustier haben oder hatten, wie heißt es? (Wenn Sie nie ein Haustier gehabt hätten, wie würden Sie es nennen, wenn Sie es hätten?) \n"
 )
 
 #4. Kombinieren Sie den Namen ihrer Stadt und ihres Haustiers und zeigen Sie ihnen ihren Bandnamen.
 users_first_band_name_suggestion = user_city +" "+ users_pet_name
 users_second_band_name_suggestion = user_city +" "+ users_name
 #5. Stellen Sie sicher, dass der Eingabecursor in einer neuen Zeile angezeigt wird:
-print("Mein erster Vorschlag für deinen Bandnamen ist " + users_first_band_name_suggestion)
-print("Mein zweiter Vorschlag für deinen Bandnamen ist " + users_second_band_name_suggestion)
+print("\nMein erster Vorschlag für deinen Bandnamen ist " + users_first_band_name_suggestion)
+print("\nMein zweiter Vorschlag für deinen Bandnamen ist " + users_second_band_name_suggestion)
