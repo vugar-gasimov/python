3c440850c01b5fb893fa22a6bf4f19bb3b581527
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-03-26 12:20:30 +0200

    Input function, and comment.

---
+++ input.py
@@ -0,0 +1,19 @@
+# input("Hallo, wie heißen sie?")
+
+print("Hallo" + " " + input("Wie heißen sie?") + "!")
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+name = input()
+print(len(name))
+
+len(input())
+
+#input + length + print
+print(len(input()))
