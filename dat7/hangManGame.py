48c4345d6079587fc5e59df0ddd7da0f36d4f588
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-04 14:26:34 +0300

    HangMan game finished.
    New files to seperate code and user firendly messages

--- dat7/hangManGame.py
+++ dat7/hangManGame.py
@@ -1,37 +1,65 @@
 import random
-
+import hangman_art
+import hangman_words
+print(hangman_art.logo)
 print("\n        Welcome to hangman game! \n")
-# chosen_level = input("Please choose a level like 'easy', 'medium', or hard' ")
-easy_word_list = ["Smile",
-"Donut",
-"Beach",
-"Jeans",
-"Whisper",
-"Melody"]
-medium_word_list = ["Rainbow",
-"Butterfly",
-"Costume",
-"Bicycle",
-"Champion",
-"Mystery"]
-hard_word_list = ["Magnificent",
-"Nevertheless",
-"Equilibrium",
-"Orchestra",
-"Archipelago",
-"Silhouette"]
-random_word = random.choice(easy_word_list).lower()
+chosen_level = input("Please choose a level like 'easy', 'medium', 'hard' or random names) ")
+
+if chosen_level == "easy":
+    random_word = random.choice(hangman_words.easy_word_list).lower() 
+elif chosen_level == "medium":
+    random_word = random.choice(hangman_words.medium_word_list).lower() 
+elif chosen_level == "hard":
+    random_word = random.choice(hangman_words.hard_word_list).lower()
+else:
+     random_word = random.choice(hangman_words.random_names).lower()
+
 random_word_len = len(random_word)
 display = ["_"] * random_word_len
+lives = 6
 print(display)
-guessed_word = input("\n Guess a word: ").lower()
-updated_display= []
-for letter in random_word:
+print(random_word)
+
+while True:
+# while not end_of_game:
+    guessed_word = input(f"\n You've {lives} lives. Guess a word: ").lower()[0]
+    found = False
     
-    if letter == guessed_word:
-        print(True)
-        updated_display += letter
+    if guessed_word in display:
+        print(f"You've already guessed '{guessed_word}'.") 
+        found = True
     else:
-        print(False)
-        updated_display += '_'
-print(updated_display)
+        for position in range(random_word_len):
+            letter = random_word[position]
+            if letter == guessed_word:
+                print(f"\n You guessed '{guessed_word}', that's in the word.")
+                display[position] = letter
+                found = True
+           
+    
+            
+    print(display)
+    
+    if not found:
+        print(f"\n You guessed '{guessed_word}', that's not in the word. You lose a life.")
+        lives -= 1
+        print(hangman_art.stages[lives])
+    # Teacher's way
+    # if guess not in chosen_word:
+    #   lives -= 1
+    #   if lives == 0:
+    #       end_of_game = True
+    #       print("You lose.")
+            
+    if display == list(random_word):
+        print(f"{' '.join(display)}")
+        print("Congratulations, you won! \n")
+        break
+    if lives == 0:
+        print("Unfortunately you lost! \n")
+        break
+    # Teacher's way
+    # if "_" not in display:
+    #   end_of_game = True
+    #   print("You win.")
+    
---
+++ dat7/hangman_words.py
@@ -0,0 +1,21 @@
+easy_word_list = ["Smile",
+"Donut",
+"Beach",
+"Jeans",
+"Whisper",
+"Melody"]
+medium_word_list = ["Rainbow",
+"Butterfly",
+"Costume",
+"Bicycle",
+"Champion",
+"Mystery"]
+hard_word_list = ["Magnificent",
+"Nevertheless",
+"Equilibrium",
+"Orchestra",
+"Archipelago",
+"Silhouette"]
+random_names = [ "Paris", "Tokyo", "Everest",
+ "Giraffe", "Narwhal", "Peacock",
+ "Pizza", "Burrito", "Macaroni"]
