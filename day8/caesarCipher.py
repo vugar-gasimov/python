3eb7ddfda198d42a10d111be26aa2eca09bb86b5
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-05 23:13:09 +0300

    Combining two functions.

--- day8/caesarCipher.py
+++ day8/caesarCipher.py
@@ -4,31 +4,57 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def caeser(plain_text, shift_amount, direction):
+    cipher_text=""
+    for char in plain_text:
+        if char in alphabet:
+            if direction == "encode":
+                shifted_index = (alphabet.index(char) + shift_amount) % len(alphabet)               
+            elif direction == "decode":
+                shifted_index = (alphabet.index(char) - shift_amount) % len(alphabet)                    
+            cipher_text += alphabet[shifted_index]
+        else:
+            cipher_text += char
+    if direction == "encode" or direction == "decode":
+        print(f"The {direction}d text is {cipher_text}")
+    else:
+        print("You didn't choose 'encode' or 'decode' (Try again).")
+caeser(plain_text=text, shift_amount=shift, direction=direction)
 
-def encrypt(plain_text, shift_amount):
-    cipher_text=""
-    for char in plain_text:
-        if char in alphabet:
-            shifted_index = (alphabet.index(char)+ shift_amount) % len(alphabet)
-            cipher_text += alphabet[shifted_index]
-        else:
-            cipher_text += char
+# def caesar(start_text, shift_amount, cipher_direction):
+#   end_text = ""
+#   if cipher_direction == "decode":
+#       shift_amount *= -1
+#   for letter in start_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     end_text += alphabet[new_position]
+#   print(f"Here's the {direction}d result: {end_text}")
+
+# def encrypt(plain_text, shift_amount):
+#     cipher_text=""
+#     for char in plain_text:
+#         if char in alphabet:
+#             shifted_index = (alphabet.index(char)+ shift_amount) % len(alphabet)
+#             cipher_text += alphabet[shifted_index]
+#         else:
+#             cipher_text += char
              
-    print(f"The encoded text is {cipher_text}")
+#     print(f"The encoded text is {cipher_text}")
     
-def decrypt(plain_text, shift_amount):
-    cipher_text=""
-    for char in plain_text:
-        if char in alphabet:
-            shifted_index = (alphabet.index(char) - shift_amount) % len(alphabet)
-            cipher_text += alphabet[shifted_index]
-        else:
-            cipher_text += char
+# def decrypt(plain_text, shift_amount):
+#     cipher_text=""
+#     for char in plain_text:
+#         if char in alphabet:
+#             shifted_index = (alphabet.index(char) - shift_amount) % len(alphabet)
+#             cipher_text += alphabet[shifted_index]
+#         else:
+#             cipher_text += char
              
-    print(f"The encoded text is {cipher_text}")
+#     print(f"The decoded text is {plain_text}")
         
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(plain_text=text, shift_amount=shift)
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#     decrypt(plain_text=text, shift_amount=shift)
     
