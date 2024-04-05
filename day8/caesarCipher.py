826fc6fd1e0cc1297d669a60754dca702da6557f
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-06 00:02:04 +0300

    Update in while loop

--- day8/caesarCipher.py
+++ day8/caesarCipher.py
@@ -2,27 +2,36 @@ import caesarCipherArt
 print(caesarCipherArt.logo)
 print("\n Welcome to caesar cipher which is a way of coding messages which have been used by Caesar \n")
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caeser(plain_text, shift_amount, direction):
-    cipher_text=""
-    for char in plain_text:
-        if char in alphabet:
-            if direction == "encode":
-                shifted_index = (alphabet.index(char) + shift_amount) % len(alphabet)               
-            elif direction == "decode":
-                shifted_index = (alphabet.index(char) - shift_amount) % len(alphabet)                    
-            cipher_text += alphabet[shifted_index]
+    def caeser(plain_text, shift_amount, direction):
+        cipher_text=""
+        for char in plain_text:
+            if char in alphabet:
+                if direction == "encode":
+                    shifted_index = (alphabet.index(char) + shift_amount) % len(alphabet)               
+                elif direction == "decode":
+                    shifted_index = (alphabet.index(char) - shift_amount) % len(alphabet)                    
+                cipher_text += alphabet[shifted_index]
+            else:
+                cipher_text += char
+        if direction == "encode" or direction == "decode":
+            print(f"The {direction}d text is {cipher_text}")
         else:
-            cipher_text += char
-    if direction == "encode" or direction == "decode":
-        print(f"The {direction}d text is {cipher_text}")
-    else:
-        print("You didn't choose 'encode' or 'decode' (Try again).")
-caeser(plain_text=text, shift_amount=shift, direction=direction)
+            print("You didn't choose 'encode' or 'decode' (Try again).")
+    caeser(plain_text=text, shift_amount=shift, direction=direction)
+    while True:
+        if_continue = input("\n Would you like to encode or decode more ? (yes/no): ").lower()
+        if if_continue == "yes":
+            break
+        elif if_continue == "no":
+            print(" \n Thanks for using our coding program and goodbye.")
+            exit()
+        else:
+            print(" \n Invalid input. Please enter 'yes' or 'no'.")
 
 # def caesar(start_text, shift_amount, cipher_direction):
 #   end_text = ""
