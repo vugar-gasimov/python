babb99c9a8f8e7950f4d2ccd7404a37771fbcf9c
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-08 12:44:33 +0300

    Learning input outputs basics and simple challange

--- day10/funcOutput.py
+++ day10/funcOutput.py
@@ -1,9 +1,13 @@
-name = input("Enter your name: ")
-surname =input("Enter your surname: ")
+# name = input("Enter your name: ")
+# surname =input("Enter your surname: ")
 
 def format_name(f_name, s_name):
+    if f_name == "" or s_name == "":
+        return "You didn't enter valid inputs."
     first_name = f_name.title()
     second_name = s_name.title()
-    return first_name, second_name
+    # return first_name, second_name
+    return f"Result: {first_name}, {second_name}"
 
-print(format_name(f_name=name, s_name=surname))
+# print(format_name(f_name=name, s_name=surname))
+print(format_name(input("Enter your name: "),input("Enter your surname: ")))
