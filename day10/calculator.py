fc4b7402d7ee1f867f9a715e61e3f61df84854b1
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-08 15:16:34 +0300

    Function desciption and calculator project.

---
+++ day10/calculator.py
@@ -0,0 +1,73 @@
+import art
+print("\n Welcome to Calculator program ")
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return(n1 * n2)
+
+def divide(n1, n2):
+    return(n1 / n2)
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+    
+}
+def calculator():
+    print(art.logo)
+    
+    num1 = float(input("What is the first number: "))
+
+    continue_answer = True
+    while continue_answer:
+        for operation in operations:
+            print(f"Choose an {operation}")
+        
+        active_operation = input("Pick an operation: ")[0]
+
+        num2 = float(input("What is the next number: "))
+
+# My way
+# def collector(num1, num2, active_operation):
+#     if active_operation == "+":
+#         return add(num1, num2)
+#     elif active_operation == "-":
+#         return subtract(num1, num2)
+#     elif  active_operation == "*":
+#         return multiply(num1, num2)
+#     elif active_operation == "/":
+#         return divide(num1, num2)
+#     else:
+#         return "You picked a not existing operation. Please pick again."
+        
+# calc_answer = collector(num1=num1, num2=num2, active_operation=active_operation)
+# Teacher's way
+        collector = operations[active_operation]
+        answer = collector(num1, num2)
+            
+        print(f"{num1} {active_operation} {num2} = {answer}")   
+        to_continue_answer = input(f"\n Type 'y' to continue calculating with {answer}, or Type 'n' to start a new calculation. ")[0].lower()
+        if to_continue_answer == "y":
+            continue_answer = True
+            num1 = answer
+        else:
+            continue_answer = False
+            calculator()
+calculator()       
+    # active_operation = input("Pick another operation: ")
+
+    # num3 = int(input("What is the next number: "))
+
+    # collector = operations[active_operation]
+    # second_answer = collector(answer, num3)
+    # print(f"{answer} {active_operation} {num3} = {second_answer}")
+    
+    # to_continue_answer = input(f"\n Type 'y' to continue calculating with {second_answer}, or Type 'n' to exit. ")[0].lower()
+
--- day10/funcOutput.py
+++ day10/funcOutput.py
@@ -2,6 +2,7 @@
 # surname =input("Enter your surname: ")
 
 def format_name(f_name, s_name):
+    """ Therefore, month_days[month - 1] calculates the correct index in the month_days list based on the input month and retrieves the corresponding number of days for that month. """
     if f_name == "" or s_name == "":
         return "You didn't enter valid inputs."
     first_name = f_name.title()
@@ -11,3 +12,4 @@ def format_name(f_name, s_name):
 
 # print(format_name(f_name=name, s_name=surname))
 print(format_name(input("Enter your name: "),input("Enter your surname: ")))
+format_name()
