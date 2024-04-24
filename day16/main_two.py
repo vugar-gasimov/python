8c3ed36a6a674f9c52ce58e041cfff091c0020fc
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-04-24 15:56:22 +0300

    Coffee Macine in OOP

--- day16/main_two.py
+++ day16/main_two.py
@@ -1,3 +1,38 @@
-from day16.menu import Menu, MenuItem
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+is_on = True
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while is_on:
+    drink_choice = input("What would you like to order? (espresso/latte/cappuccino/): ")
+
+    if drink_choice == "off":
+        print("You turned off the coffee machine.")
+        is_on = False
+
+    elif drink_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+        
+    else:
+        found_drink = menu.find_drink(drink_choice)
+     
+            
+        if found_drink:
+            enough_resource = coffee_maker.is_resource_sufficient(found_drink)
+            print(f"You have ordered a {found_drink.name}. The price is ${found_drink.cost}.")
+           
+            if enough_resource:
+                if money_machine.make_payment(found_drink.cost):
+                    coffee_maker.make_coffee(found_drink)
+               
+        else:
+            print(f"Sorry, the '{drink_choice}' is not available.")
+            
+    
+    
