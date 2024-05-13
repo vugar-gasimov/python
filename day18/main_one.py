479edf761ba407f85fc8e6546d557a81dc01c8f6
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-05-13 16:30:14 +0300

    Lerning day 18

--- day18/main_one.py
+++ day18/main_one.py
@@ -1,5 +1,22 @@
 from turtle import Turtle, Screen
+import heroes
+print(heroes.gen())
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+for _ in range(4):
+    tim.right(90)
+    tim.forward(100)
+    
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
 
-timmy_the_turtle = Turtle()
+
 screen = Screen()
 screen.exitonclick()
