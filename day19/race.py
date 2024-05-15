ab7ba6d39a9fcc615336794f073c0fbad4bb9160
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-05-15 13:26:28 +0300

    Turtle race complete. Start of day 20.

--- day19/race.py
+++ day19/race.py
@@ -19,7 +19,7 @@ tim.pendown()
 tim.goto(x=320, y=-160)
 tim.penup()
 tim.goto(x=-380, y=-160)
-
+race_is_on = False
 tim.pendown()
 # tim.showturtle()
 tim.speed(15)
@@ -27,14 +27,33 @@ user_bet = screen.textinput(title= "Make your bet.", prompt="Which turtle will w
 print(user_bet)
 start_position = -380
 start_line = -140
+all_turtles = []
 for color in colors:
     random_speed = random.randint(1, 8)
     new_turtle = Turtle(shape="turtle")
+    new_turtle.hideturtle()
     new_turtle.speed(random_speed)
     new_turtle.penup()
     new_turtle.color(color)
     new_turtle.goto(x=start_position, y= start_line)
     start_line += 55
-    new_turtle.forward(760)
-
+    all_turtles.append(new_turtle)
+    new_turtle.showturtle()
+    # new_turtle.forward(760)
+if user_bet:
+    race_is_on = True
+    
+while race_is_on:
+    
+    for turtle in all_turtles:
+        if turtle.xcor() > 300:
+            race_is_on = False
+            winning_turtle = turtle.pencolor()
+            if winning_turtle == user_bet:
+                print(f"You've won! The winning turtle is  {winning_turtle}!.")
+            else:
+                print(f"You've lost! The winning turtle is {winning_turtle}!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+    
 screen.exitonclick()
