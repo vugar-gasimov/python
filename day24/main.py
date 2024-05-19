1fac6efa1bd50b074d61667594be42ff69183927
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-05-20 01:15:22 +0300

    Day 24 Open, Read, Write, Append, Close file with with open.

---
+++ day24/main.py
@@ -0,0 +1,13 @@
+file_path = "day24\my_file.txt"
+new_file_path = "day24\my_new_file.txt"
+# with open(file_path, mode=r) as file:
+# # file = open(file_path)
+#     print(file)
+#     file_content = file.read()
+#     print(file_content)
+# # file.close()
+
+# with open(file_path, mode="a") as file:
+#     file.write("\n New text.")
+# with open(new_file_path, mode="w") as file:
+#     file.write("\n New text.")
