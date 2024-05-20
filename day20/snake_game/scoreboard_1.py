b773c605a216ae11f06fdbe98b086a827812bf29
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-05-20 03:15:14 +0300

    Learning and practicong file pathes and also in snake game saving high score forever in data.txt file.

--- day20/snake_game/scoreboard_1.py
+++ day20/snake_game/scoreboard_1.py
@@ -1,4 +1,9 @@
 from turtle import Turtle
+# r_data = open("day20\snake_game\data.txt", mode="r")
+# w_data = open("day20\snake_game\data.txt", mode="w")
+# data_high_score = r_data.read()
+# print(data_high_score)
+# with open("snake_game\data.txt", mode="r") as data:
 ALIGNMENT = 'center'
 FONT = ('Courier', 20, 'normal')
 class Scoreboard(Turtle):
@@ -6,13 +11,22 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
         self.color("gold")
         self.penup()
         self.goto(0, 265)
         self.hideturtle()
+        self.high_score = self.read_high_score()
         self.speed("fastest")
         self.update_score()
+        
+    def read_high_score(self):
+        with open("day20\snake_game\data.txt", mode="r") as data:
+            # self.high_score = int(data.read())
+            high_score_str = data.read().strip()
+            if high_score_str.isdigit():
+                return int(high_score_str)
+            else:
+                return 0
         
         
     def update_score(self):
@@ -21,7 +35,10 @@ class Scoreboard(Turtle):
         
     def reset_score(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            self.high_score = self.score    
+            with open("day20\snake_game\data.txt", mode="w") as data:
+                # data.write(str(self.high_score))
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
             
--- day24/main.py
+++ day24/main.py
@@ -1,10 +1,17 @@
-file_path = "day24\my_file.txt"
-new_file_path = "day24\my_new_file.txt"
-# with open(file_path, mode=r) as file:
+file_path = "../../../Desktop/my_file.txt"
+"../../../../Documents/GitHub/python/day24/main.py"
+"../../../../Desktop/my_file.txt"
+"../../../Desktop/my_file.txt"
+# C:\Users\vuqar\OneDrive\Documents\GitHub\python\day24
+# file_path = r"\Users\vuqar\OneDrive\Desktop/my_file.txt"
+# file_path = "C:/Users/vuqar/OneDrive/Desktop/my_file.txt"
+# file_path = "day24\my_file.txt"
+# new_file_path = "day24\my_new_file.txt"
+with open(file_path, mode="r") as file:
 # # file = open(file_path)
-#     print(file)
-#     file_content = file.read()
-#     print(file_content)
+    print(file)
+    file_content = file.read()
+    print(file_content)
 # # file.close()
 
 # with open(file_path, mode="a") as file:
