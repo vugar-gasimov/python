d98ab6c528fd4111685aa931c6ba62deb8e219f3
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-06-16 16:42:08 +0300

    Adding pixel with todays date time.

--- day37/habit_tracker/main.py
+++ day37/habit_tracker/main.py
@@ -1,8 +1,9 @@
 import requests
-
+from datetime import datetime
 USERNAME="vgasimov"
 TOKEN="dsG6d1asGds1G5das5dH1"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+CHESS_GRAPH = "graph1"
 
 user_params = {
     "token": TOKEN,
@@ -14,8 +15,8 @@ user_params = {
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
 
-GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 graph_config = {
     "id": "graph1",
     "name": "Chess Playing and Learning",
@@ -28,5 +29,18 @@ hearder = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=hearder)
-print(response.text)
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=hearder)
+# print(response.text)
+
+PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{CHESS_GRAPH}"
+today = datetime.now()
+# If we want yesterday
+# today = datetime(year=2024, month=6, day=15)
+today = today.date().strftime("%Y%m%d")
+print(today)
+pixel_config = {
+    "date": f"{today}",
+    "quantity": "2",
+}
+# response = requests.post(url=PIXEL_ENDPOINT, json=pixel_config, headers=hearder)
+# print(response.text)
