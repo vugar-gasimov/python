0bff55032d389920af51e8c10f5dd71343629297
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-06-17 11:54:34 +0300

    Day 37 finish adding pixel updating pixel and deleting pixel.

--- day37/habit_tracker/main.py
+++ day37/habit_tracker/main.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import json
 USERNAME="vgasimov"
 TOKEN="dsG6d1asGds1G5das5dH1"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
@@ -38,9 +39,30 @@ today = datetime.now()
 # today = datetime(year=2024, month=6, day=15)
 today = today.date().strftime("%Y%m%d")
 print(today)
+quantity = input("How many chess sessions did you do today?")
+user_msg = input("What would you like to mention about your sessions?")
 pixel_config = {
     "date": f"{today}",
-    "quantity": "2",
+    "quantity": quantity,
+    "optionalData": json.dumps({
+        "message": user_msg
+    })
 }
-# response = requests.post(url=PIXEL_ENDPOINT, json=pixel_config, headers=hearder)
+response = requests.post(url=PIXEL_ENDPOINT, json=pixel_config, headers=hearder)
+print(response.text)
+
+UPDATE_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{CHESS_GRAPH}/20240616"
+
+update_config = {
+    "quantity": "19",
+    "optionalData": "{\"message\": \"Yesterday I did 19 chess sessions.\"}"
+}
+# response = requests.put(url=UPDATE_ENDPOINT, json=update_config, headers=hearder)
+# print(response.text)
+
+
+DELETE_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{CHESS_GRAPH}/20240617"
+
+
+# response = requests.delete(url=DELETE_ENDPOINT,  headers=hearder)
 # print(response.text)
